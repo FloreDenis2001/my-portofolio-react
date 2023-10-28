c7368a85c3fc48dd91fa61ae4bc15594c8f886d1
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-28 15:25:47 +0300

    update_nav

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiHomeAlt2 , BiSolidBookBookmark, BiUser} from 'react-icons/bi';
 import {LuHeartHandshake} from 'react-icons/lu';
 import {RiContactsBookFill} from 'react-icons/ri';
 const NavigationBar = () => {
+
+    let [activeNav,setActiveNav]=useState('#home')
   return (
     <aside className="navigation__mobile">
     <ul className="navigation__mobile__list">
         <li>
-            <a className="navigation__mobile__link__active" href="#home"><BiHomeAlt2/></a>
+            <a  href="#home" onClick={()=>setActiveNav('#home')} className={activeNav === "#home" ? "navigation__mobile__link navigation__active" : 'navigation__mobile__link'}><BiHomeAlt2/></a>
         </li>
 
         <li>
-            <a className="navigation__mobile__link" href="#about"> <BiUser/></a>
+            <a  onClick={()=>setActiveNav('#about')} className={activeNav === "#about" ? "navigation__mobile__link navigation__active" : 'navigation__mobile__link'} href="#about"> <BiUser/></a>
         </li>
 
         <li>
-            <a href="#experience" className="navigation__mobile__link"><BiSolidBookBookmark/></a>
+            <a href="#experience"  onClick={()=>setActiveNav('#experience')} className={activeNav === "#experience" ? "navigation__mobile__link navigation__active" : 'navigation__mobile__link'}><BiSolidBookBookmark/></a>
         </li>
 
         <li>
-            <a href="#projects" className="navigation__mobile__link"><LuHeartHandshake/> </a>
+            <a href="#projects"  onClick={()=>setActiveNav('#projects')} className={activeNav === "#projects" ? "navigation__mobile__link navigation__active" : 'navigation__mobile__link'}><LuHeartHandshake/> </a>
         </li>
 
         <li>
