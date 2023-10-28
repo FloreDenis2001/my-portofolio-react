00c807640603ac56442387a6e4d784d08d302feb
FloreDenis2001 <floredenis907@yahoo.com>
2023-10-28 15:03:48 +0300

    last_update

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,23 @@ import Experience from './components/experience/Experience';
 import Projects from './components/projects/Projects';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
-
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
+        <Helmet>
+          <title>Flore Denis</title>
+          <meta name="description" content="Helmet application" />
+        </Helmet>
+
       <Header />
       <Hero />
       <About />
-      <Experience/>
-      <Projects/>
-      <Contact/>
-      <Footer/>
+      <Experience />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
